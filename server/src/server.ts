9648b67765b7b94b6542a706e32889b9fad6af5e
Allan Galdino <allan.g.galdino@outlook.com>
2020-10-13 20:38:36 -0300

    putting views to control the response

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import path from 'path';
 import './database/connection';
 
 import routes from './routes'
@@ -7,8 +7,8 @@ import routes from './routes'
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Rota que a aplicação estará hospedada
 app.listen(3333, () => {
