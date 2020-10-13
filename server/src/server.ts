bc1f072c5ade26872418eedb95e06aa9e83cfd93
Allan Galdino <allan.g.galdino@outlook.com>
2020-10-13 09:05:35 -0300

    server initialized

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express, { json } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.json({ message: 'Hello World!' });
+});
+
+// Rota que a aplicação estará hospedada
+app.listen(3333, () => {
+  console.log("🚀 Server running on port 3333!");
+});
